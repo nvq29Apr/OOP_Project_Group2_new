4289a4f5ff9432ca30f2024caecd53b94fc0a0a3
quyetdaika <nvq29apr@gmail.com>
2023-12-24 01:27:39 +0700

    make look better & exit button

--- src/main/java/views/Home.java
+++ src/main/java/views/Home.java
@@ -5,6 +5,7 @@
 package views;
 
 import dataprocessors.GetDataFromJson;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
@@ -15,7 +16,7 @@ import models.NFT;
  * @author quyetdaika
  */
 public class Home extends javax.swing.JFrame implements GetDataFromJson {
-
+    
     /**
      * Creates new form Home
      */
@@ -24,6 +25,7 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
         defaultTableModel = (DefaultTableModel)jTable1.getModel();
         nfts = getNFTsFromJson();
         btnTimeSlotClicked(btn1H.getText());
+//        jScrollPane1.setVisible(false);
     }
 
     /**
@@ -38,35 +40,66 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
         header = new javax.swing.JPanel();
         jlbHome = new javax.swing.JLabel();
         jlbNFT = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jlbNFT1 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        btnExit = new javax.swing.JButton();
+        filterPanel = new javax.swing.JPanel();
         btnSearch = new javax.swing.JButton();
         jtfSearch = new javax.swing.JTextField();
         btn1H = new javax.swing.JButton();
         btn6H = new javax.swing.JButton();
         btn24H = new javax.swing.JButton();
         btn7D = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        header.setBackground(new java.awt.Color(204, 204, 255));
+        header.setBackground(new java.awt.Color(204, 255, 204));
+        header.setMinimumSize(new java.awt.Dimension(785, 57));
+        header.setName(""); // NOI18N
 
         jlbHome.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jlbHome.setText("Home");
+        jlbHome.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jlbHomeMouseClicked(evt);
+            }
+        });
 
         jlbNFT.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jlbNFT.setText("NFT");
+
+        jlbNFT1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jlbNFT1.setText("Tweet");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel1.setText("Blog");
+
+        btnExit.setBackground(new java.awt.Color(255, 153, 153));
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout headerLayout = new javax.swing.GroupLayout(header);
         header.setLayout(headerLayout);
         headerLayout.setHorizontalGroup(
             headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(headerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jlbHome)
+                .addGap(28, 28, 28)
+                .addComponent(jlbHome, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jlbNFT)
-                .addGap(191, 191, 191))
+                .addGap(28, 28, 28)
+                .addComponent(jlbNFT1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addGap(54, 54, 54)
+                .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         headerLayout.setVerticalGroup(
             headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -74,9 +107,92 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
                 .addGap(15, 15, 15)
                 .addGroup(headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlbHome)
-                    .addComponent(jlbNFT))
-                .addContainerGap(15, Short.MAX_VALUE))
+                    .addComponent(jlbNFT)
+                    .addComponent(jlbNFT1)
+                    .addComponent(jLabel1)
+                    .addComponent(btnExit))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
+
+        btnSearch.setBackground(new java.awt.Color(255, 255, 204));
+        btnSearch.setText("Search");
+        btnSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSearchActionPerformed(evt);
+            }
+        });
+
+        jtfSearch.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jtfSearchKeyPressed(evt);
+            }
+        });
+
+        btn1H.setBackground(new java.awt.Color(204, 255, 255));
+        btn1H.setText("1H");
+        btn1H.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn1HActionPerformed(evt);
+            }
+        });
+
+        btn6H.setBackground(new java.awt.Color(204, 255, 255));
+        btn6H.setText("6H");
+        btn6H.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn6HActionPerformed(evt);
+            }
+        });
+
+        btn24H.setBackground(new java.awt.Color(204, 255, 255));
+        btn24H.setText("24H");
+        btn24H.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn24HActionPerformed(evt);
+            }
+        });
+
+        btn7D.setBackground(new java.awt.Color(204, 255, 255));
+        btn7D.setText("7D");
+        btn7D.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn7DActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout filterPanelLayout = new javax.swing.GroupLayout(filterPanel);
+        filterPanel.setLayout(filterPanelLayout);
+        filterPanelLayout.setHorizontalGroup(
+            filterPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, filterPanelLayout.createSequentialGroup()
+                .addComponent(btnSearch)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jtfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 243, Short.MAX_VALUE)
+                .addComponent(btn1H, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btn6H, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btn24H, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btn7D, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        filterPanelLayout.setVerticalGroup(
+            filterPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(filterPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(filterPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn7D)
+                    .addComponent(btn24H)
+                    .addComponent(btnSearch)
+                    .addComponent(jtfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn6H)
+                    .addComponent(btn1H))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jScrollPane1.setEnabled(false);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -106,80 +222,27 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
         jTable1.setShowGrid(true);
         jScrollPane1.setViewportView(jTable1);
 
-        btnSearch.setText("Search");
-        btnSearch.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSearchActionPerformed(evt);
-            }
-        });
-
-        btn1H.setText("1H");
-        btn1H.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn1HActionPerformed(evt);
-            }
-        });
-
-        btn6H.setText("6H");
-        btn6H.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn6HActionPerformed(evt);
-            }
-        });
-
-        btn24H.setText("24H");
-        btn24H.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn24HActionPerformed(evt);
-            }
-        });
-
-        btn7D.setText("7D");
-        btn7D.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn7DActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
+                .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(filterPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane1)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnSearch)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jtfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 117, Short.MAX_VALUE)
-                        .addComponent(btn1H, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn6H, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn24H, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btn7D, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(1, 1, 1)))
-                .addContainerGap(22, Short.MAX_VALUE))
+                    .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(header, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnSearch)
-                    .addComponent(jtfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btn6H)
-                    .addComponent(btn24H)
-                    .addComponent(btn7D)
-                    .addComponent(btn1H))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 303, Short.MAX_VALUE)
+                .addComponent(filterPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -215,6 +278,30 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
                 defaultTableModel.addRow(x.getNFT());
         }
     }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void jlbHomeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlbHomeMouseClicked
+        // TODO add your handling code here:
+        filterPanel.setVisible(false);
+        jScrollPane1.setVisible(false);
+        
+    }//GEN-LAST:event_jlbHomeMouseClicked
+
+    private void jtfSearchKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfSearchKeyPressed
+        // TODO add your handling code here:
+        if(evt.getKeyCode() ==  KeyEvent.VK_ENTER){
+            if(jtfSearch.getText().equals("")) return;
+            defaultTableModel.setRowCount(0);
+            for(NFT x : currentNfts){
+                if(x.getName().toLowerCase().contains(jtfSearch.getText().toLowerCase())) 
+                    defaultTableModel.addRow(x.getNFT());
+            }
+        }
+    }//GEN-LAST:event_jtfSearchKeyPressed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_btnExitActionPerformed
     
     private void btnTimeSlotClicked(String timeSlot){
         currentNfts = getNFTsByTimeslot(timeSlot);
@@ -273,12 +360,16 @@ public class Home extends javax.swing.JFrame implements GetDataFromJson {
     private javax.swing.JButton btn24H;
     private javax.swing.JButton btn6H;
     private javax.swing.JButton btn7D;
+    private javax.swing.JButton btnExit;
     private javax.swing.JButton btnSearch;
+    private javax.swing.JPanel filterPanel;
     private javax.swing.JPanel header;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JLabel jlbHome;
     private javax.swing.JLabel jlbNFT;
+    private javax.swing.JLabel jlbNFT1;
     private javax.swing.JTextField jtfSearch;
     // End of variables declaration//GEN-END:variables
 }
