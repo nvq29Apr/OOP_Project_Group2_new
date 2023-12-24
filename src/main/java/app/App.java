1ed476c1554a8e99c406b7635e1709412460b290
quyetdaika <nvq29apr@gmail.com>
2023-12-25 06:46:30 +0700

    uml

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import collectors.nft.NFTsCollector;
+import collectors.post.BlogsCollector;
 import collectors.post.TweetsCollector;
 import java.io.IOException;
 import views.Home;
@@ -11,11 +12,14 @@ import views.Home;
  */
 public class App {
     public static void main(String[] args) throws InterruptedException, IOException {
-//        NFTsCollector nftCollector = new NFTsCollector();
-//        nftCollector.active();
-//
-//        TweetsCollector tweetCollector = new TweetsCollector();
-//        tweetCollector.active();
+        NFTsCollector nftCollector = new NFTsCollector();
+        nftCollector.active();
+
+        TweetsCollector tweetCollector = new TweetsCollector();
+        tweetCollector.active();
+        
+        BlogsCollector blogCollector = new BlogsCollector();
+        blogCollector.active();
         
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
